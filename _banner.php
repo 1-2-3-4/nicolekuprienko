9cfecfb0389c0c6f6a8de17fb177592685dd3712
Nicole Kuprienko <nicole.kuprienko@gmail.com>
2016-02-02 19:35:35 -0500

    Make banner div link to home pg

--- _banner.php
+++ _banner.php
@@ -1,4 +1,4 @@
-		<section class="slider">
+		<section class="slider" onclick="window.open('index.php');">
             <div class="slide slide1 active">
                 <h1>Nicole Kuprienko</h1>
                 <h2>Web Design &amp; Front-End Dev</h2>
